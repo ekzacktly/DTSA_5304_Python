5872fd9a492cc457ce12ac1c0cf762001b3c9bcf
ekzacktly <zro5001@gmail.com>
2025-06-18 22:28:46 -0400

    Changes

--- Final_Poject_Viz.py
+++ Final_Poject_Viz.py
@@ -47,46 +47,61 @@ for i in loss_by_year['Year']:
     j += 1
 
 # Build Plot
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
+fig2, ax2 = plt.subplots()
 
 
-def loss_plot(xvals, yvals, curr_ax):
-    colors = []
-    for x in yvals:
-        if x == yvals.max():
-            colors.append('red')
-        elif x == yvals.min():
-            colors.append('green')
-        else:
-            colors.append('gray')
-    curr_ax.clear()
-    curr_ax.bar(xvals, yvals, color=colors)
-    ax.set_title('Loss by Year')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Loss (lbs)')
-    ax.set_xlim([xvals.min() - 1, xvals.max() + 1])
-    fig.subplots_adjust(bottom=0.25, left=0.25)
-    plt.show()
+# def loss_plot(xvals, yvals, curr_ax, curr_fig):
+#     colors = []
+#     for x in yvals:
+#         if x == yvals.max():
+#             colors.append('red')
+#         elif x == yvals.min():
+#             colors.append('green')
+#         else:
+#             colors.append('gray')
+#     curr_ax.clear()
+#     curr_ax.bar(xvals, yvals, color=colors)
+#     curr_ax.set_title('Loss by Year')
+#     curr_ax.set_xlabel('Year')
+#     curr_ax.set_ylabel('Loss (lbs)')
+#     curr_ax.set_xlim([xvals.min() - 1, xvals.max() + 1])
+#     curr_fig.subplots_adjust(bottom=0.25, left=0.25)
+#     plt.show()
+#
+#
+# loss_plot(loss_by_year['Year'], loss_by_year['YearlyLoss'], ax, fig)
 
 
-loss_plot(loss_by_year['Year'], loss_by_year['YearlyLoss'], ax)
-
-
-def dispo_plot(xvals, yvals, curr_ax):
-    ops = []
+def dispo_plot(xvals, num_ops, curr_ax, curr_fig):
+    ops = pd.DataFrame(columns = ['Year', 'DispositionedBy', 'Dispositioned'])
     for x in xvals:
-
+        temp = issues[issues['IssueDate'].dt.year == x]['DispositionedBy'].value_counts()
+        for i in range(num_ops):
+            ops.loc[len(ops)] = [x, temp.index[i], temp[temp.index[i]]]
+    op_colors = {}
+    colors = [plt.cm.get_cmap('viridis')(i) for i in range(len(issues['DispositionedBy'].unique()))]
+    for i, op in enumerate(issues['DispositionedBy'].unique()):
+        op_colors[op] = colors[i]
     curr_ax.clear()
-    curr_ax.bar(xvals, yvals, color=colors)
-    ax.set_title('Loss by Year')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Loss (lbs)')
-    ax.set_xlim([xvals.min() - 1, xvals.max() + 1])
-    fig.subplots_adjust(bottom=0.25, left=0.25)
+    for year in ops['Year'].unique():
+        bottom_value = 0
+        for dispod_by in ops[ops['Year'] == year]['DispositionedBy']:
+            curr_ax.bar(year, ops[(ops['Year'] == year) & (ops['DispositionedBy'] == dispod_by)]['Dispositioned'],
+                        bottom=bottom_value, label=dispod_by, color=op_colors[dispod_by])
+            bottom_value += dispod_by
+    curr_ax.set_title('Dispos by Year')
+    curr_ax.set_xlabel('Year')
+    curr_ax.set_ylabel('Dispositions')
+    curr_ax.set_xlim([xvals.min() - 1, xvals.max() + 1])
+    curr_fig.subplots_adjust(bottom=0.25, left=0.25)
     plt.show()
 
 
-axyears = fig.add_axes([0.25, 0.05, 0.5, 0.03])
+dispo_plot(issues['IssueDate'].dt.year.unique(), 5, ax2, fig2)
+
+
+axyears = fig2.add_axes([0.25, 0.05, 0.5, 0.03])
 year_slider = RangeSlider(
     ax=axyears,
     label="Years",
@@ -101,7 +116,8 @@ def update_x_limits(val):
     new_min, new_max = val
     new_xvals = loss_by_year[(loss_by_year['Year'] >= new_min) & (loss_by_year['Year'] <= new_max)]['Year']
     new_yvals = loss_by_year[(loss_by_year['Year'] >= new_min) & (loss_by_year['Year'] <= new_max)]['YearlyLoss']
-    loss_plot(new_xvals, new_yvals, ax)
+    # loss_plot(new_xvals, new_yvals, ax, fig)
+    dispo_plot(new_xvals, 5, ax2, fig2)
 
 
 year_slider.on_changed(update_x_limits)
